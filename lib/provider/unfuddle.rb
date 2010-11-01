a058dd39c7caaf400b3c8a412d0ed7b37ea8fead
Alexander Sviridov <alexander.sviridov@gmail.com>
2010-11-01 19:36:37 +0300

    Added :protocol parameter to auth (required for accounts with "Force SSL" option)

--- lib/provider/unfuddle.rb
+++ lib/provider/unfuddle.rb
@@ -18,6 +18,7 @@ module TicketMaster::Provider
       if auth.account.nil? or auth.username.nil? or auth.password.nil?
         raise "Please provide at least an account (subdomain), username and password)"
       end
+      UnfuddleAPI.protocol = auth.protocol if auth.protocol?
       UnfuddleAPI.account = auth.account || auth.subdomain
       UnfuddleAPI.authenticate(auth.username, auth.password)
     end
--- spec/ticketmaster-unfuddle_spec.rb
+++ spec/ticketmaster-unfuddle_spec.rb
@@ -8,4 +8,9 @@ describe "Ticketmaster::Provider::Unfuddle" do
     @ticketmaster.should be_a_kind_of(TicketMaster::Provider::Unfuddle)
   end
 
+  it "should allow to use SSL" do
+    @ticketmaster = TicketMaster.new(:unfuddle, {:account => 'ticketmaster', :username => 'foo', :password => '000000', :protocol => 'https'})
+    @ticketmaster.provider::TICKET_API.site.should be_an_instance_of(URI::HTTPS)
+    @ticketmaster.provider::PROJECT_API.site.should be_an_instance_of(URI::HTTPS)
+  end
 end
